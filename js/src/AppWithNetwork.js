cba862bcc074223d1613d015d6299db4e76aa0a1
Ethan Butler <e.thom.butler@gmail.com>
2019-04-06 10:38:41 -0400

    Update AppWithNetwork.js

--- js/src/AppWithNetwork.js
+++ js/src/AppWithNetwork.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import App from "./App";
 
+// Authentication header for fetch
 const headers = {
   'X-WP-Nonce': window.__tokens__.wordpress,
   'Content-Type': 'application/json',
 }
 
+// Makes data from REST response fit the format of our attributes
 const normalizeResponse = data => ({
   title: data.title.rendered,
   artist: data.meta.artist,
@@ -13,6 +15,7 @@ const normalizeResponse = data => ({
   thumbnail_uri: data.meta.thumbnail_uri,
 })
 
+// Makes our attributes fit the format needed by the REST API format
 const normalizeRequest = attributes => ({
   title: attributes.title,
   content: attributes.description,
@@ -24,24 +27,32 @@ const normalizeRequest = attributes => ({
 
 class AppWithNetwork extends React.Component {
   componentDidMount = () => {
+    // If we have an id in our attributes, GET initial data
     if(this.props.attributes.id) {
       this.handleGet(this.props.attributes.id)
     }
   }
 
   handleGet = async id => {
+    // Make authenticated GET request
     const res = await fetch(`/wp-json/wp/v2/albums/${id}`, {
       headers,
     })
+    // Get block attributes from REST API
     const attributes = normalizeResponse(await res.json())
+    // Update block attributes
     this.props.setAttributes(attributes)
   }
 
   handleConfirm = async e => {
     const { attributes, setAttributes, onConfirm } = this.props
+    
+    // If we have an ID, update the resource. Otherwise, create a new resource
     const endpoint = attributes.id
       ? `/wp-json/wp/v2/albums/${attributes.id}`
       : '/wp-json/wp/v2/albums'
+    
+    // Make a POST request with our attributes
     const res = await fetch(endpoint, {
       method: 'POST',
       headers,
@@ -49,8 +60,14 @@ class AppWithNetwork extends React.Component {
         normalizeRequest(attributes)
       ),
     })
+    
+    
     const { id } = await res.json()
+    
+    // Update attributes with ID if it doesn't exist
     setAttributes({ id })
+    
+    // Call our original onConfirm callback
     onConfirm(e)
   }
 
